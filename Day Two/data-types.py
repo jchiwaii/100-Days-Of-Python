598b84e123e7a5046bc053855b07afd990d6038b
Chiwai <chiwai.kiriba@gmail.com>
2024-01-23 15:47:45 +0300

    Day 2 of 100 Days of Python

---
+++ Day Two/data-types.py
@@ -0,0 +1,29 @@
+#Data Types
+
+#String
+
+print("Hello"[4]) #Subscripts a string according to the number in the brackets
+
+#Prints specific letters
+street_name = "Kenyatta Road"
+print(street_name[2] + street_name[7])
+
+#Anything in double quotations is a string 
+print("123" + "345") 
+print("False")
+
+#Integer - Numbers with no decimal places
+
+print(123+345) #Actual numbers
+
+123_345_567 #Visualize large numbers. _ is used in place of ,
+
+#Float - Contains a decimal place
+
+3.4555
+
+#Boolean - Only two possible values
+
+True
+False
+
