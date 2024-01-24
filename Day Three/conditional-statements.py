ee1832414b9d6fbb792c662410eff3f90fd21fd1
Chiwai <chiwai.kiriba@gmail.com>
2024-01-24 17:47:43 +0300

    Day 3 of 100 Days of Python

--- Day Three/conditional-statements.py
+++ Day Three/conditional-statements.py
@@ -5,10 +5,16 @@ height = int(input("What is your height in cm? "))
 
 if height > 120:
     print("You can continue to ride")
+    age = int(input("How old are you?"))
+    if age < 12:
+        print("You can pay $5")
+    elif age <= 18:
+        print("You can pay $7")
+    else:
+        print("You can pay $12")
 else:
     print("Sorry, not possible")
     
-# Other sytanxes include
 # > = : greater than
 # < = : less than
 # = = : equal to
