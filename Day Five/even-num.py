8d5e91dc7b813469bed09a4791e806ffba8a5b43
Chiwai <chiwai.kiriba@gmail.com>
2024-01-27 14:03:34 +0300

    Day 5 of 100 days of Python

---
+++ Day Five/even-num.py
@@ -0,0 +1,6 @@
+even_num = 0
+for num in range(1, 101):
+    if (num % 2) == 0:
+        even_num += num
+
+print(even_num)
--- Day Five/for-loops.py
+++ Day Five/for-loops.py
@@ -4,3 +4,14 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit + " Pie")
 
+
+for number in range (1, 10):
+    print(number)
+
+for number in range (1, 10, 4): # Four steps in the range
+    print(number)
+
+total = 0
+for num in range(1, 101):
+    total += num
+print(total)
